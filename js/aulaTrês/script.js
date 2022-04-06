63766b11aaa199679c9541f2de47eeaa67fb14e9
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-05 22:51:59 -0300

    aula três

---
+++ js/aulaTrês/script.js
@@ -0,0 +1,12 @@
+let nota1 = 7;
+let nota2 = 7;
+let nota3 = 5;
+let nota4 = 9;
+
+let media = (nota1+nota2+nota3+nota4)/4;
+
+if(media >= 7) {
+    alert(`Você está aprovado com média ${media}`)
+} else {
+    alert(`Você está reprovado com média ${media}`)
+}
