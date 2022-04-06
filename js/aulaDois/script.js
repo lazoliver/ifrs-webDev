0b028fb7e657329e459b886f91cbff1264cfc6c0
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-05 22:37:41 -0300

    aula dois

---
+++ js/aulaDois/script.js
@@ -0,0 +1,13 @@
+let a = 2;
+let b = 3;
+let c = a + b; /* adição */
+/*
+let c = a / b; - divisão
+let c = a * b; - multiplicação
+let c = a - b; - subtração
+*/
+alert(c);
+
+let nome;
+nome = prompt("Informe seu nome");
+alert(`Olá, ${nome}`)
