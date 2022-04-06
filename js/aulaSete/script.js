7f58f5a3c51a8f301a5ff556f3ae107f55723a17
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-06 00:02:40 -0300

    aula sete

---
+++ js/aulaSete/script.js
@@ -0,0 +1,7 @@
+function carreguei() {
+    document.getElementById("log").innerHTML += "<br>Carreguei";
+}
+
+function redimensionei() {
+    document.getElementById("log").innerHTML += "<br>Redimensionei"
+}
