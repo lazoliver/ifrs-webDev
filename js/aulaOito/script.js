e0a4f6c92eb0054304011a8d74228e1ba631ce05
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-06 01:18:42 -0300

    aula oito

---
+++ js/aulaOito/script.js
@@ -0,0 +1,9 @@
+function umClique() {
+    document.getElementById("img").src="black-panter.jpg";
+    document.getElementById("log").innerHTML += `Logged`
+}
+
+function doisCliques() {
+    document.getElementById("img").src="";
+    document.getElementById("log").innerHTML += `LogOff`
+}
