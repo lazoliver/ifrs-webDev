ffe14c14e225ecef1f01e1024a7adfd5d1520e5b
lazoliver <wilian.oliveiractt@gmail.com>
2022-04-10 21:29:06 -0300

    aula nove

---
+++ js/aulaNove/script.js
@@ -0,0 +1,32 @@
+function submeti() {
+    document.getElementById("log").innerHTML += "submeti<br>";
+    return false
+}
+
+function resetei() {
+    document.getElementById("log").innerHTML += "resetei<br>";
+}
+
+function saiDoCampo() {
+    document.getElementById("log").innerHTML += "sai do campo<br>";
+}
+
+function mudei() {
+    document.getElementById("log").innerHTML += "mudei<br>";
+}
+
+function entreiNoCampo() {
+    document.getElementById("log").innerHTML += "entrei no campo<br>";
+}
+
+function selecionei() {
+    document.getElementById("log").innerHTML += "selecionei<br>";
+}
+
+function teclaBaixo() {
+    document.getElementById("log").innerHTML += "tecla pressionada<br>";
+}
+
+function teclaCima() {
+    document.getElementById("log").innerHTML += "tecla solta<br>";
+}
